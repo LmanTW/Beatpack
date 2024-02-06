0c3ef6186e799bc9141088853b5a686d9294282b
LmanTW <82919705+LmanTW@users.noreply.github.com>
2024-02-06 23:11:26 +0800

    Update Build Script

--- Scripts/Build/Main.js
+++ Scripts/Build/Main.js
@@ -1,13 +1,35 @@
 const { api } = require('@electron-forge/core')
+const { zip } = require('zip-a-folder')
 const path = require('path')
+const fs = require('fs')
 
-api.package({
-  m
-})
+// Build
+async function build (platforms) {
+  await api.package({
+    dir: path.resolve(__dirname, '../../Beatpack'),
 
-//const { spawn } = require('child_process')
+    platform: platforms,
 
-//const childProcess = spawn('npm', ['run', 'build'])
+    interactive: true,
+    outDir: path.join(__dirname, 'Cache')
+  })
 
-//childProcess.stdout.on('data', (data) => console.log(data.toString()))
-//childProcess.stderr.on('data', (data) => console.log(data.toString()))
+  //await api.make({
+  //  dir: path.resolve(__dirname, '../../Beatpack'),
+
+  //  platform: 'darwin',
+
+  //  interactive: true,
+  //  outDir: path.join(__dirname, 'Cache')
+  //})
+
+  if (!fs.existsSync(path.resolve(__dirname, '../../Builds'))) fs.mkdirSync(path.resolve(__dirname, '../../Builds'))
+
+  if (platforms.includes('win32')) fs.renameSync(path.join(__dirname, 'Cache', 'Beatpack-win32-x64', 'Beatpack.exe'), path.resolve(__dirname, `../../Builds/Beatpack.exe`))
+
+  if (platforms.includes('darwin')) await zip(path.join(__dirname, 'Cache', 'Beatpack-darwin-x64', 'Beatpack.app'), path.resolve(__dirname, `../../Builds/Beatpack.zip`), { destPath: 'Beatpack.app/' })
+
+  fs.rmSync(path.join(__dirname, 'Cache'), { recursive: true })
+}
+
+build(['darwin'])
--- Scripts/Test/Main.js
+++ Scripts/Test/Main.js
@@ -1,6 +1,7 @@
 const { spawn } = require('child_process')
+const path = require('path')
 
-const childProcess = spawn('npm', ['start'])
+const childProcess = spawn('npm', ['run', 'start'])
 
 childProcess.stdout.on('data', (data) => console.log(data.toString()))
 childProcess.stderr.on('data', (data) => console.log(data.toString()))
