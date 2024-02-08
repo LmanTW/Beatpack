79988286be8dff8569a04581b44609555253a7dd
LmanTW <82919705+LmanTW@users.noreply.github.com>
2024-02-08 16:05:05 +0800

    Update Build Script

--- Scripts/Build/Main.js
+++ Scripts/Build/Main.js
@@ -26,10 +26,11 @@ async function build (platforms) {
   if (!fs.existsSync(path.resolve(__dirname, '../../Builds'))) fs.mkdirSync(path.resolve(__dirname, '../../Builds'))
 
   if (platforms.includes('win32')) fs.renameSync(path.join(__dirname, 'Cache', 'Beatpack-win32-x64', 'Beatpack.exe'), path.resolve(__dirname, `../../Builds/Beatpack.exe`))
-
   if (platforms.includes('darwin')) await zip(path.join(__dirname, 'Cache', 'Beatpack-darwin-x64', 'Beatpack.app'), path.resolve(__dirname, `../../Builds/Beatpack.zip`), { destPath: 'Beatpack.app/' })
 
   fs.rmSync(path.join(__dirname, 'Cache'), { recursive: true })
 }
 
 build(['darwin'])
+
+// build(['win32'])
--- Scripts/Test/Main.js
+++ Scripts/Test/Main.js
@@ -1,6 +1,8 @@
 const { spawn } = require('child_process')
 
-const childProcess = spawn('npm', ['run', 'start'])
+const childProcess = spawn('npm', ['run', 'start'], {
+  cwd: __dirname
+})
 
 childProcess.stdout.on('data', (data) => console.log(data.toString()))
 childProcess.stderr.on('data', (data) => console.log(data.toString()))
